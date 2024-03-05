cf507022b42bc5c20295e263fac12fdcd93a1459
Thomas Waldmann <tw@waldmann-edv.de>
2024-03-05 11:22:51 +0100

    adopt pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-"""
-setup for vmnlcli package
-"""
-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 with open('README.md') as f:
@@ -14,43 +10,6 @@ with open('README.md') as f:
 
 
 setup(
-    name='vmnlcli',
-    version='0.2.0',
-    url='http://github.com/thomaswaldmann/velomobielnl/',
-    license='MIT',
-    author='Thomas Waldmann',
-    author_email='twaldmann@thinkmo.de',
-    description='command line interface for some dutch velomobile websites',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    keywords="velomobile odometer cli velomobiel.nl intercitybike.nl welmers.net",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'vmnlcli = vmnlcli:main',
-        ]
-    },
-    platforms='any',
-    setup_requires=[],
-    install_requires=[
-        'requests',
-    ],
-    python_requires='>=3.9',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Utilities',
-        'Topic :: Database :: Front-Ends',
-        'Topic :: Internet :: WWW/HTTP :: Site Management',
-    ],
 )
