51ac14fd520790959391d4b5599e256d1b9efe84
Thomas Waldmann <tw@waldmann-edv.de>
2024-03-05 11:01:30 +0100

    require python >= 3.9

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         'requests',
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -45,10 +45,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Utilities',
         'Topic :: Database :: Front-Ends',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
